5340a0a52ad73ccf34f30595ea4a608377cf1f75
do-rh <87504110+do-rh@users.noreply.github.com>
2021-08-27 15:50:30 -0700

    avg pair & can-construct complete

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,27 @@
+"use strict";
+/** Takes inputs of 1 sorted array of integers & 1 target average and evaluates whether there are 2 integers in the array that average to the target average.
+ * Use 2 pointers to compare against average
+ * 1) Make left (first array element) & right (last array element) pointer
+ * 2) Calculate average of 2 pointers (their value) & compare against the target average while right pointer > left pointer
+ *    - if equals target, return true
+ *    - if greater, move right pointer left
+ *    - if smaller, move left pointer to right
+ * 3) return false
+*/
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(integers, targetAvg) {
+    let left = 0;
+    let right = integers.length-1;
+    while (left < right) {
+        if (((integers[left] + integers[right]) / 2) === targetAvg) {
+            return true;
+        }
+        if (((integers[left] + integers[right]) / 2) < targetAvg) {
+            left++;
+        }
+        if (((integers[left] + integers[right]) / 2) > targetAvg) {
+            right--;
+        }
+    }
+    return false;
 }
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,36 @@
+"use strict";
+/** Accepts 1 word and 1 string of letters, returns true if the string has all the letters in the word
+ * returns false if letters are missing.
+ * 1) If letterString length < word, return false
+ * 2) Make frequency counter of word characters
+ * 3) Make frequency counter of letterString
+ * 4) Loop over word Obj
+ *  4a) If key DNE in letterObj, return false
+ *  4b) If key exists in letterOBJ 
+ *      - value in letterObj is less than value in wordObj, return false
+ * 5) Loop ends, return true  
+ */
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+
+function canConstructWord(word, letters) {
+    if (letters.length < word.length) {
+        return false;
+    }
+    let wordObj = makeFrequencyCounter(word);
+    let lettersObj = makeFrequencyCounter(letters);
+    for (let char in wordObj) {
+        if (lettersObj[char] === undefined || lettersObj[char] < wordObj[char]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/** Accepts a string input and returns a frequency counter of the characters as an object) */
+function makeFrequencyCounter(string) {
+    let charObj = {};
+    for (let char of string) {
+        charObj[char] = (charObj[char] || 0) + 1;
+    }
+    return charObj
 }
