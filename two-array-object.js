0cf299f92652be3117ad56f13fa6355b08fb141f
kimberly lin <kimberly.lin10@gmail.com>
2021-08-27 16:04:35 -0700

    completed two array objects

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,26 @@
+"use strict";
+
+/** Accepts 2 arrays of varying length. 
+ * Return object with keys from array1, paired with values from array2.
+ * If there are more keys than values, values are null.
+ * If there are more values than keys, values will not be in object.
+ */
+
+/**
+ * 1. Create empty object
+ * 2. Loop through array 1 and 2 at the same time and insert in object
+ * 2.a) If element in array2 does not exist, set value as null.
+ * 3. Return object
+*/
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+    let paired={};
+    for (let i=0; i<keys.length; i++){
+        if (values[i]===undefined){
+            paired[keys[i]]= null;
+        } else {
+            paired[keys[i]]= values[i];
+        }
+    }
+    return paired;
 }
